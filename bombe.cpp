ce48c0dd962410e9cad969e892a7c7f341132f56
nckuwater <F74081195@gs.ncku.edu.tw>
2020-06-30 17:36:20 +0800

    find_loop output correct, but need to remove duplicate cases.

--- bombe.cpp
+++ bombe.cpp
@@ -2,9 +2,11 @@
 #include <vector>
 #include <array>
 #include <fstream>
+#include <algorithm>
 #include "enigma.h"
 
 using namespace std;
+
 
 class bombe_rotor{
     /* each bombe_rotor have one enigma circuit */
@@ -15,6 +17,7 @@ class bombe_rotor{
         static vector<array<int, 2>> bombe_menu[26];
         static vector<vector<int>> vec_loops;
         void add_text_to_bombe_menu(string plain, string cipher){
+            // array<int, 2> = {step, letter_index}
             const char *pptr = plain.c_str();
             const char *cptr = cipher.c_str();
             for (int i = 0; i < plain.size(); ++i){
@@ -24,10 +27,10 @@ class bombe_rotor{
         }
         void find_loops(){
             remain_letters.resize(25);
-            vector<int> process;
+            vector<int> process, base_process;
             vector < array<int, 2>> *menu_ptr;
             vector<vector<int>> vec_process;
-            int letter_index;
+            int letter_index, find_index;
             for (int i = 1; i < 26; ++i){
                 // A is not need to be add
                 remain_letters[i] = i;
@@ -35,31 +38,53 @@ class bombe_rotor{
             vec_process.assign(1, {0});
             while(!vec_process.empty()){
                 for (int i = 0; i < vec_process.size(); ++i){
-                    letter_index = *vec_process.at(i).rbegin();
-                    process = vec_process.at(i);
+                    letter_index = (vec_process.at(i).at(vec_process.at(0).size()-1));
                     menu_ptr = &bombe_menu[letter_index];
-                    vec_process.at(i).push_back((*menu_ptr)[0][0]);
-                    vec_process.at(i).push_back((*menu_ptr)[0][1]);
-                    for (int route = 1; route < (*menu_ptr).size(); ++route){
-                        process.push_back((*menu_ptr)[route][0]);
-                        process.push_back((*menu_ptr)[route][1]);
-                        if(is_value_exists(letter_index, process)){
-                            // find closed loop, append into vec_loops, remove the base process from vec_process.
-                            vec_loops.push_back(vector<int>(process.begin() + i, process.end()));
-                            vec_process.erase(vec_process.begin() + i);
-                            --i;
-                        }else{
+                    base_process = vec_process.at(i);
+                    vec_process.erase(vec_process.begin() + i);
+                    --i;
+                    cout << i << " " << letter_index << endl;
+                    //vec_process.at(i).push_back((*menu_ptr)[0][0]);
+                    //vec_process.at(i).push_back((*menu_ptr)[0][1]);
+                    for (int route = 0; route < (*menu_ptr).size(); ++route){
+                        if((*menu_ptr).at(route)[0] == *(base_process.end()-2))
+                            continue;
+                        process = base_process;
+                        process.push_back((*menu_ptr).at(route)[0]);
+                        process.push_back((*menu_ptr).at(route)[1]);
+                        for (int j = 0; j < process.size(); j+=2){
+                            cout << static_cast<char>(process[j] + 65) << " : " << process[j] << endl;
+                        }
+                        find_index = is_value_exists((*menu_ptr)[route][1], process);
+                        if (find_index != -1)
+                        {
+                            // find closed loop, append into vec_loops.
+                            process = sort_loop(process);
+                            if(!is_loop_exists(process))
+                                vec_loops.push_back(vector<int>(process.begin() + find_index, process.end()));
+                        }
+                        else
+                        {
                             // not form closed loop yet, append into vec_process and keep trying.
                             vec_process.push_back(process);
                         }
-
+                    }
+                }                
+            }
+            for (int i = 0; i < vec_loops.size(); ++i){
+                for (int k = 0; k < vec_loops[i].size(); ++k){
+                    if(k%2==0){
+                        cout << static_cast<char>(vec_loops[i][k] + 65) << " ";
+                    }
+                    else{
+                        cout << vec_loops[i][k] << " ";
                     }
                 }
-                
+                cout << endl;
             }
         }
         int is_value_exists(int value, const vector<int> &loop){
-            for (int i = 0; i < loop.size(); i+=2){
+            for (int i = 0; i < loop.size()-1; i+=2){
                 /* check every letter value, not steps value */
                 if(value == loop[i])
                     return i;
@@ -87,18 +112,24 @@ class bombe_rotor{
                 return loop;
             new_loop.assign(loop.begin() + min_index, loop.end());
             new_loop.insert(new_loop.end() ,loop.begin() + 1, loop.begin() + min_index +1);
-            for (int i = 0; i < new_loop.size(); ++i){
+            if(new_loop[1] > *(new_loop.end()-2))
+                reverse(new_loop.begin(), new_loop.end());
+            /*for (int i = 0; i < new_loop.size(); ++i){
                 cout << new_loop[i] << " ";
             }
-            cout << endl;
+            cout << endl;*/
             return new_loop;
         }
 
-
 };
-
+vector<array<int, 2>> bombe_rotor::bombe_menu[] = {};
+vector<vector<int>> bombe_rotor::vec_loops = vector<vector<int>>();
 int main(){
     bombe_rotor br;
+    for (int i = 0; i < 26; ++i){
+        br.bombe_menu[i] = vector<array<int, 2>>(0);
+    }
+    br.add_text_to_bombe_menu("WSNPNLKLSTCS", "ATTACKATDAWN");
     br.find_loops();
     return 0;
 }
