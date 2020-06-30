040761a338fd30a2e36928436b5a41125dabdaa5
nckuwater <F74081195@gs.ncku.edu.tw>
2020-06-30 15:09:04 +0800

    build bombe menu function base.

--- bombe.cpp
+++ bombe.cpp
@@ -9,6 +9,30 @@ class bombe_rotor{
     public:
         enigma enigma_machine;
         int total_tested_init_steps_set;
+        static vector<int[2]> bombe_menu[26];
+        static vector<vector<int>> vec_loops;
+        void add_text_to_bombe_menu(string plain, string cipher){
+            const char *pptr = plain.c_str();
+            const char *cptr = cipher.c_str();
+            for (int i = 0; i < plain.size(); ++i){
+                bombe_menu[pptr[i] - 65].push_back({i, cptr[i] - 65});
+                bombe_menu[cptr[i] - 65].push_back({i, pptr[i] - 65});
+            }
+        }
+        void find_loops(){
+
+        }
+        void sort_loop(vector<int> loop){
+            int min_num = loop[0], min_index = 0;
+            for (int i = 2; i < loop.size(); i+=2){
+                if(loop[i] < min_num){
+                    min_num = loop[i];
+                    min_index = i;
+                }
+            }
+
+        }
+
 };
 
 int main(){
